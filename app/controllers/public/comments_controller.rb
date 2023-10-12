13534fa184f10251ef4fa16bb858a2d8372b6f01
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-10-12 07:09:30 +0000

    comment set

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,7 +1,49 @@
 class Public::CommentsController < ApplicationController
   def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    @comment = current_customer.comments.new(comment_params)
+    @comment.post_id = params[:post_id]
+    if @comment.save
+      flash[:notice] = "コメントしました。"
+      redirect_to post_path(@comment.post_id)
+    else
+      render :new
+    end
   end
 
   def edit
+    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:notice] = "コメントを編集しました。"
+      redirect_to post_path(@comment.post_id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      flash[:notice] = "コメントを削除しました。"
+      redirect_to post_path(@comment.post_id)
+    else
+      flash[:notice] = "コメントの削除に失敗しました。"
+      redirect_to post_path(@comment.post_id)
+    end
+  end
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -20,6 +20,7 @@ class Public::PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 
   def edit
