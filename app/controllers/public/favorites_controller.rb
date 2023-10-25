a4712ad5738f659ec19c05cf8d8556bfab0f7cdd
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-10-25 08:02:47 +0000

    shat design

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -6,18 +6,18 @@ class Public::FavoritesController < ApplicationController
   end
   
   def create
+    @post = Post.find(params[:post_id])
     favorite = current_customer.favorites.new
-    favorite.post_id = params[:post_id]
+    favorite.post_id = @post.id
     favorite.save
     post = Post.find(params[:post_id])
     post.create_notification_like!(current_customer)
-    redirect_to request.referer
   end
   
   def destroy
-    favorite = current_customer.favorites.find_by(post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
+    favorite = current_customer.favorites.find_by(post_id: @post.id)
     favorite.destroy
-    redirect_to request.referer
   end
   
 end
