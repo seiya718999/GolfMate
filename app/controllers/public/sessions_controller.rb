38185c3d822879fa9c15bcb8acad732fc1da0dc1
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-10-27 07:09:52 +0000

    design

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :customer_state, only: [:create]
+  
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted
+      flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
+      redirect_to new_customer_registration_path
+    end
+  end
+  
   def after_sign_in_path_for(resource)
       customer_path(resource)
   end
