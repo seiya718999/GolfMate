cb3314913ac0a517556fd7dfde7a2106f92e2aa4
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-10-28 06:04:53 +0000

    bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,22 +16,3 @@ import "../stylesheets/application";
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-document.addEventListener("DOMContentLoaded", function() {
-  const postImageInput = document.getElementById('post_image');
-  const imagePreview = document.getElementById('image-preview');
-
-  postImageInput.addEventListener('change', function() {
-    const file = this.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-        imagePreview.setAttribute('src', e.target.result);
-        imagePreview.style.display = 'block';
-      }
-      reader.readAsDataURL(file);
-    } else {
-      imagePreview.style.display = 'none';
-    }
-  });
-});
